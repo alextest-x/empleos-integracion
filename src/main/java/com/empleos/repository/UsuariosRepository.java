18fba1fde1c0a46ff65e9670ba70f1abfce596a6
alextest-x <192.168.000.x.@gmail.com>
2024-07-05 00:32:47 -0600

    update empleos integracion

---
+++ src/main/java/com/empleos/repository/UsuariosRepository.java
@@ -0,0 +1,11 @@
+package com.empleos.repository;
+
+import com.empleos.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuariosRepository extends JpaRepository<Usuario, Integer> {
+
+
+
+
+}
